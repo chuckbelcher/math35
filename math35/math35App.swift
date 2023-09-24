7525cc234540dd7f5ac36c2553efdd2667f02b20
Chuck Belcher <cfbelcher@pateogroup.com>
2023-09-23 21:58:22 -0400

    Initial Commit

---
+++ math35/math35App.swift
@@ -0,0 +1,17 @@
+//
+//  math35App.swift
+//  math35
+//
+//  Created by Chuck Belcher on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct math35App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
