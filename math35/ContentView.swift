5beed512bcce938fd574a7823227b486ab4f846d
Chuck Belcher <cfbelcher@pateogroup.com>
2023-09-25 22:49:23 -0400

    Added initial logic to show question

--- math35/ContentView.swift
+++ math35/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @State private var level = "easy"
     @State private var ttable = 2
-    @State private var numQuestions = 5
-    @State private var easyNumbers = [1,2,5,10]
-    @State private var confidentNumbers = [3,4,9,11]
-    @State private var hardNumbers = [6,7,8,9,12]
+    @State private var numQuestions = 2
+    @State private var easy = [1,2,5,10]
+    @State private var confident = [3,4,9,11]
+    @State private var hard = [6,7,8,9,12]
+    @State private var currentQuestion = 10
+    @State private var currentAnswer = ""
     
     var body: some View {
         VStack {
@@ -50,7 +52,7 @@ struct ContentView: View {
                             .foregroundColor(.blue)
                             .padding(.leading)
                     }
-                    Stepper(value: $numQuestions, in: 5...15, step: 5) {
+                    Stepper(value: $numQuestions, in: 2...6, step: 2) {
                         Text("Number of Questions: \(numQuestions)")
                             .foregroundColor(.blue)
                             .padding(.leading)
@@ -58,32 +60,82 @@ struct ContentView: View {
                 }
             }
             .bold()
-            Spacer()
+
             
             //Show Questions
+            VStack (alignment: .leading) {
+                Section ("Play Your Game") {
+                    HStack {
+                        //TODO: Add if ready to play and start button
+                        Button {
+                            getNumber()
+                        } label: {
+                            Text("New Question")
+                                .padding(.leading)
+                                .font(.headline)
+                            
+                        }
+                        Spacer()
+                        Text("\(ttable)  X \(currentQuestion) = ")
+                            .font(.title3)
+                        
+                        TextField("?", text: $currentAnswer)
+                            .font(.title2)
+                            .frame(width: 40)
+                        Button {
+                            print(currentAnswer)
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .font(.headline)
+                            Text("Check")
+                                .bold()
+                            
+                        }
+                    }
+                }
+                Spacer()
+
+            }
+            .bold()
+            .padding(.top, 20)
             
+            Spacer()
+            //TODO: Add Text for Result
             
-            //Add Text for Result
-            
-            //Show Score
+            //TODO: Show Score
             
             //Add Reset / play again button.
             Button {
                 print("Playing level \(level) for table \(ttable)")
             } label: {
-                Text("Play Again")
-                    .padding(.vertical, 20)
-                    .padding(.horizontal, 50)
-                    .background(Color(.blue))
-                    .foregroundColor(.white)
-                    .bold()
-                    .cornerRadius(30)
+                    Text("Play Again")
+                        .padding(.vertical, 20)
+                        .padding(.horizontal, 50)
+                        .background(Color(.blue))
+                        .foregroundColor(.white)
+                        .bold()
+                        .cornerRadius(30)
+                
             }
 
             
             
         }
         .padding()
+        .onAppear(perform: getNumber)
+            
+        
+    }
+    
+    func getNumber() {
+
+        if level == "easy" {
+            currentQuestion = easy.randomElement() ?? 0
+        } else if level == "confident" {
+            currentQuestion = confident.randomElement() ?? 0
+        } else {
+            currentQuestion = hard.randomElement() ?? 0
+        }
     }
 }
 
